0b57c8ed1d5bf8898cbc4993b4fb24e3ac9681db
herlianali <aliyasa58@gmail.com>
2021-06-04 17:38:02 +0700

    new update 0.1

---
+++ daftar_psb.php
@@ -0,0 +1,3 @@
+<?php
+	echo "daftar_psb";
+?>
--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home Page</title>
+</head>
+<body>
+	<center>	
+		<h1>Logo Dan Nama Sekolah</h1>
+		<a href="index.php">Home</a>
+		<a href="profile.php">Profile</a>
+		<a href="pengumuman.php">Pengumuman</a>
+		<a href="berita.php">Berita</a>
+		<a href="fasilitas.php">Fasilitas</a>
+		<a href="daftar_psb.php">Daftar PSB</a>
+		<a href="data_psb.php">Data PSB</a>
+		<a href="hasil_test.php">Hasil Tes</a>
+		<a href="login.php">Login</a>
+	</center>
+</body>
+</html>
