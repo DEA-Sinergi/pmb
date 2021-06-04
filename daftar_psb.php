0747c8a9b4d5c3486230a8389e7359294e15a1ba
herlianali <aliyasa58@gmail.com>
2021-06-04 18:54:41 +0700

    psb form

--- daftar_psb.php
+++ daftar_psb.php
@@ -1,3 +1,19 @@
 <?php
 	echo "daftar_psb";
 ?>
+<h3>Isi Data PSB</h3>
+<form action="" method="POST">
+	Nama : <input type="text" name="nama"><br>
+	No Nisn : <input type="text" name="no_nisn"><br>
+	Alamat : <input type="text" name="alamat"><br>
+	No Telp : <input type="text" name="no_telp"><br>
+	Asal Sekolah : <input type="text" name="asal_sekolah"><br>
+	Jenis Kelamin : <input type="text" name="jenis_kelamin"><br>
+	Agama : <input type="text" name="agama"><br>
+	Tempat Tanggal Lahir : <input type="text" name="tem_lahir"><input type="date" name="tang_lahir"><br>
+	Nama Orang Tua : <input type="text" name="n_ortu"><br>
+	Pekerjaan : <input type="text" name="pekerjaan"><br>
+	Program Pilihan : <input type="text" name="prog_pilihan"><br>
+	Agama : <input type="text" name="agama"><br>
+	<button type="submit">Simpan</button>
+</form>
